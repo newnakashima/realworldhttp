c3f31fcdc717b5191ca4bccc852b86ca559d4f3f
newnakashima <newnakashima2014@gmail.com>
2017-08-20 17:11:27 +0900

    2.6.1

--- digest_server.go
+++ digest_server.go
@@ -6,11 +6,17 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"io/ioutil"
-	"github.com/k0kibun/pp"
+	"github.com/k0kubun/pp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	dump, err := httputil.DumpRequest(r, true)
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+    fmt.Println(string(dump))
+    fmt.Println(w, "<html><body>Hello</body></html>\n")
 }
 
 func handlerDigest(w http.ResponseWriter, r *http.Request) {
@@ -31,5 +37,10 @@ func handlerDigest(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
+    var httpServer http.Server
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/digest", handlerDigest)
+    log.Println("start http listening :18888")
+    httpServer.Addr = ":18888"
+    log.Println(httpServer.ListenAndServe())
 }
