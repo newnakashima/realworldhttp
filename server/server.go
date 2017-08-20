423930fe0d4646cd8c48c293bee8c25be39bfc34
newnakashima <newnakashima2014@gmail.com>
2017-08-20 21:04:26 +0900

    3.3

---
+++ server/server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"io/ioutil"
+	"github.com/k0kubun/pp"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, err := httputil.DumpRequest(r, true)
+    if err != nil {
+        log.Println("error!")
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    } else {
+        log.Println("ok!")
+    }
+
+    // fmt.Println(string(dump))
+    fmt.Fprintf(w, "<html><body>Hello</body></html>\n")
+}
+
+func handlerDigest(w http.ResponseWriter, r *http.Request) {
+	pp.Printf("URL: %s\n", r.URL.String())
+	pp.Printf("Query: %v\n", r.URL.Query())
+	pp.Printf("Proto: %s\n", r.Proto)
+	pp.Printf("Method: %s\n", r.Method)
+	pp.Printf("Header: %s\n", r.Header)
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("--body--\n%s\n", string(body))
+	if _, ok := r.Header["Authorization"]; !ok {
+		w.Header().Add("WWW-Authenticate", `Digest realm="Secret Zone, nonce="12345", algorithm=MD5, qop="auth"`)
+		w.WriteHeader(http.StatusUnauthorized)
+	} else {
+		fmt.Fprintf(w, "<html><body>secret page</body></htm>")
+	}
+}
+
+func main() {
+    var httpServer http.Server
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/digest", handlerDigest)
+    log.Println("start http listening :18888")
+    httpServer.Addr = ":18888"
+    log.Println(httpServer.ListenAndServe())
+}
