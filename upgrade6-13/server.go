c92db4345357e022291154a735909716068f2af0
newnakashima <newnakashima2014@gmail.com>
2017-10-18 22:10:05 +0900

    6-14 upgrade protocol

--- upgrade6-13/server.go
+++ upgrade6-13/server.go
@@ -4,8 +4,8 @@ import (
     "fmt"
     "log"
     "net/http"
-    "net/http/httputil"
-    "io/ioutil"
+    "io"
+    "time"
 )
 
 func handleUpgrade(w http.ResponseWriter, r *http.Request) {
@@ -22,4 +22,31 @@ func handleUpgrade(w http.ResponseWriter, r *http.Request) {
         return
     }
     defer conn.Close()
+    response := http.Response{
+        StatusCode: 101,
+        Header: make(http.Header),
+    }
+    response.Header.Set("Upgrade", "MyProtocol")
+    response.Header.Set("Connection", "Upgrade")
+    response.Write(conn)
+
+    for i := 1; i <= 10; i++ {
+        fmt.Fprintf(readWriter, "%d\n", i)
+        fmt.Println("->", i)
+        readWriter.Flush()
+        recv, err := readWriter.ReadBytes('\n')
+        if err == io.EOF {
+            break
+        }
+        fmt.Printf("<- %s", string(recv))
+        time.Sleep(500 * time.Millisecond)
+    }
+}
+
+func main() {
+    var httpServer http.Server
+    http.HandleFunc("/", handleUpgrade)
+    log.Println("start http listening :18888")
+    httpServer.Addr = ":18888"
+    log.Println(httpServer.ListenAndServe())
 }
